5722139a75f71962bfce70d1e44469c1870440f4
doug@luna <doug@totem9.net>
2018-07-28 02:16:15 +0100

    compiler emitter

--- compiler/parser.hpp
+++ compiler/parser.hpp
@@ -7,10 +7,14 @@ using namespace std;
 static int pos = 0;
 
 //-- next token functions
+static const Node EOF_NODE = { "EOF", "EOF", -1 };
 static const Node& nexttok() {
-	static const Node nEOF = { "EOF", "EOF", -1 };
-	if (pos >= (int)toklist.size())  return nEOF;
+	if (pos >= (int)toklist.size())  return EOF_NODE;
 	return toklist[pos];
+}
+static const Node& lasttok() {
+	if (pos <= 0)  return EOF_NODE;
+	return toklist[pos-1];
 }
 static int expect(const string& type, const string& val) {
 	if (nexttok().type == "EOF")  return 0;
@@ -25,40 +29,65 @@ static int require(const string& type, const string& val) {
 	return 1;
 }
 
+//-- emitor
+static void emit(const vector<string>& args) {
+	for (auto& a : args)
+		cout << a << " ";
+	cout << endl;
+}
+
 //-- parser functions
 static void parse_expression();
 
 static void parse_factor() {
-	if      (expect("identifier", "")) { }
-	else if (expect("number", "")) { }
+	if      (expect("identifier", "")) { emit({ "LOD", lasttok().val }); }
+	else if (expect("number", "")) { emit({ "LIT", lasttok().val }); }
 	else if (expect("operator", "(")) { parse_expression();  require("operator", ")"); }
 	else 	{ throw string("unexpected token in factor"); }
 }
 
 static void parse_term() {
 	parse_factor();
-	while (expect("operator", "*") || expect("operator", "/"))
+	while (expect("operator", "*") || expect("operator", "/")) {
+		auto& op = lasttok();
 		parse_factor();
+		emit({ "OPR", op.val });
+	}
 }
 
 static void parse_expression() {
-	// throw string("unimplemented: expression");
-	expect("operator", "+") || expect("operator", "-");  // either or none
-	parse_term();
-	while (expect("operator", "+") || expect("operator", "-"))
+	// add or sub
+	if (expect("operator", "+") || expect("operator", "-")) {
+		auto& op = lasttok();
 		parse_term();
+		emit({ "OPR", op.val });
+	}
+	// none - just parse term
+	else {
+		parse_term();
+	}
+	// if add or sub, loop
+	while (expect("operator", "+") || expect("operator", "-")) {
+		auto& op = lasttok();
+		parse_term();
+		emit({ "OPR", op.val });
+	}
 }
 
 static void parse_statement() {
 	// printf("parsing statement...\n");
 	// assign
 	if (expect("identifier", "")) {
+		auto& id = lasttok();
 		require("operator", ":=");
 		parse_expression();
+		emit({ "STO", id.val });
 	}
 	// call
 	if (expect("keyword", "call")) {
 		require("identifier", "");
+		auto& id = lasttok();
+		emit({ "CAL", id.val });
 	}
 	// begin / end block
 	if (expect("keyword", "begin")) {
@@ -71,7 +100,10 @@ static void parse_statement() {
 	// while
 	// read / write
 	if (expect("keyword", "read") || expect("keyword", "write")) {
+		auto& cmd = lasttok().val;
 		require("identifier", "");
+		auto& id = lasttok();
+		emit({ "EXT", cmd, id.val });
 	}
 	// null - actually ok
 }
@@ -82,8 +114,11 @@ static void parse_block() {
 		// printf("parsing const block...\n");
 		while (true) {
 			require("identifier", "");
+			auto& id = lasttok();
 			require("operator", "=");
 			require("number", "");
+			auto& num = lasttok();
+			emit({ "CON", id.val, num.val });
 			if (!expect("operator", ","))  break;
 		}
 		require("operator", ";");
@@ -93,6 +128,8 @@ static void parse_block() {
 		// printf("parsing var block...\n");
 		while (true) {
 			require("identifier", "");
+			auto& id = lasttok();
+			emit({ "VAR", id.val });
 			if (!expect("operator", ","))  break;
 		}
 		require("operator", ";");
@@ -101,6 +138,8 @@ static void parse_block() {
 	while (expect("keyword", "procedure")) {
 		// printf("parsing procedure block...\n");
 		require("identifier", "");
+		auto& id = lasttok();
+		emit({ "proc", id.val });
 		require("operator", ";");
 		parse_block();
 		require("operator", ";");
